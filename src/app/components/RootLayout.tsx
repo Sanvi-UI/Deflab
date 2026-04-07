40f1c772944a563f8fd0db8727cbd1812b7e566e
Sanvi-UI <sanvikwatra1804@gmail.com>
2026-04-07 19:18:11 +0530

    Fix duplicate footer on home route

--- src/app/components/RootLayout.tsx
+++ src/app/components/RootLayout.tsx
@@ -1,15 +1,18 @@
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 import { Navbar } from "./Navbar";
 import { Footer } from "./Footer";
 
 export function RootLayout() {
+  const { pathname } = useLocation();
+  const isHomePage = pathname === "/";
+
   return (
     <div className="min-h-screen flex flex-col bg-white">
       <Navbar />
       <main className="flex-1">
         <Outlet />
       </main>
-      <Footer />
+      {!isHomePage && <Footer />}
     </div>
   );
 }
